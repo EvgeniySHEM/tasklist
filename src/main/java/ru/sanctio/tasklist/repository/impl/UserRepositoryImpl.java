24730f03423acab60119752de5ef6bd00ff3aa5b
Evgeniy Sharychenkov <sanctio@yandex.ru>
2024-01-07 08:11:55 +0300

    Add classes TaskRepositoryImpl and UserRepositoryImpl

---
+++ src/main/java/ru/sanctio/tasklist/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package ru.sanctio.tasklist.repository.impl;
+
+import org.springframework.stereotype.Repository;
+import ru.sanctio.tasklist.domain.user.Role;
+import ru.sanctio.tasklist.domain.user.User;
+import ru.sanctio.tasklist.repository.UserRepository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void create(User user) {
+
+    }
+
+    @Override
+    public void insertUserRole(Long userId, Role role) {
+
+    }
+
+    @Override
+    public boolean isTaskOwner(Long userId, Long taskId) {
+        return false;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
