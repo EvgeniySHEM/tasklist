18ffc3f1bdda6cdc699bb1aeb470ac6218f926f7
Evgeniy Sharychenkov <sanctio@yandex.ru>
2024-01-09 00:08:54 +0300

    Реализовал методы login и refresh в AuthServiceImpl

--- src/main/java/ru/sanctio/tasklist/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/sanctio/tasklist/service/impl/AuthServiceImpl.java
@@ -1,21 +1,42 @@
 package ru.sanctio.tasklist.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+import ru.sanctio.tasklist.domain.user.User;
 import ru.sanctio.tasklist.service.AuthService;
+import ru.sanctio.tasklist.service.UserService;
 import ru.sanctio.tasklist.web.dto.auth.JwtRequest;
 import ru.sanctio.tasklist.web.dto.auth.JwtResponse;
+import ru.sanctio.tasklist.web.security.JwtTokenProvider;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private final AuthenticationManager authenticationManager;
+
+    private final UserService userService;
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        JwtResponse jwtResponse = new JwtResponse();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
+                loginRequest.getPassword()));
+        User user = userService.getUserByUsername(loginRequest.getUsername());
+        jwtResponse.setId(user.getId());
+        jwtResponse.setUsername(user.getUsername());
+        jwtResponse.setAccessToken(jwtTokenProvider.createAccessToken(user.getId(),
+                user.getUsername(), user.getRoles()));
+        jwtResponse.setRefreshToken(jwtTokenProvider.createRefreshToken(user.getId(), user.getUsername()));
+        return jwtResponse;
     }
 
     @Override
     public JwtResponse refresh(String refreshToken) {
-        return null;
+        return jwtTokenProvider.refreshUserToken(refreshToken);
     }
 }
